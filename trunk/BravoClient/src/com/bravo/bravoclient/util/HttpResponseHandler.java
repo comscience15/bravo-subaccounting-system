ed6c4f32e0350b9b668f9baf7d459cfd2252f072
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2013-11-09 00:47:17 +0000

    modified the http response handler, change to static, and added a new overloaded method to avoid "multiple consuming issue" for response. Please be careful, the http response can only be consumed once

--- trunk/BravoClient/src/com/bravo/bravoclient/util/HttpResponseHandler.java
+++ trunk/BravoClient/src/com/bravo/bravoclient/util/HttpResponseHandler.java
@@ -9,6 +9,8 @@ import org.apache.http.HttpResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
 /**
  * This class is going to handle the response from server
  * @author Daniel
@@ -17,11 +19,44 @@ import org.json.JSONObject;
 public class HttpResponseHandler {
 	
 	/**
+	 * This method can not be consumed for multiple times, can only used once
 	 * @param response HttpResponse object from backend server
 	 * @param parameterName The name of parameter inside of json-like string response
 	 * @return return the value of json parameter
 	 */
-	public String parseJson(HttpResponse httpResponse, String parameterName) {
+	public static String parseJson(HttpResponse httpResponse, String parameterName) {
+		
+		String jsonInput = toString(httpResponse);
+		try {
+			JSONObject jsonObj = new JSONObject(jsonInput);
+			return jsonObj.getString(parameterName);
+		} catch (JSONException e) {
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * This method is used to parse JSON value from JSON String input
+	 * @param JSONString
+	 * @param parameterName
+	 * @return
+	 */
+	public static String parseJson(String JSONString, String parameterName) {
+		try {
+			JSONObject jsonObj = new JSONObject(JSONString);
+			return jsonObj.getString(parameterName);
+		} catch (JSONException e) {
+			return null;
+		}
+	}
+	
+	/**
+	 * Convert the response to String
+	 * @param httpResponse
+	 * @return
+	 */
+	public static String toString(HttpResponse httpResponse) {
 		InputStream inputStream = null;
 		try {
 			inputStream = httpResponse.getEntity().getContent();
@@ -30,7 +65,6 @@ public class HttpResponseHandler {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		
 		
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 		StringBuilder sb = new StringBuilder();
@@ -44,14 +78,7 @@ public class HttpResponseHandler {
 			e1.printStackTrace();
 		}
 		
-		String jsonInput = sb.toString();
-		try {
-			JSONObject jsonObj = new JSONObject(jsonInput);
-			return jsonObj.getString(parameterName);
-		} catch (JSONException e) {
-			return null;
-		}
-		
+		return sb.toString();
 	}
 
 }
