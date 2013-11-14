c9f686a74f06ecb3de54885da47801c1d36052e6
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2013-11-14 09:03:46 +0000

    Created card model and sqlite helper for data persistence purpose. We are going to use local database for data storage

---
+++ trunk/BravoClient/src/com/bravo/bravoclient/model/Card.java
@@ -0,0 +1,40 @@
+package com.bravo.bravoclient.model;
+
+public class Card {
+	private String cardId;
+	private int loyaltyPoint;
+	private String merchantAccNo;
+	private double balance;
+	
+	public String getCardId() {
+		return cardId;
+	}
+	
+	public void setCardId(String cardId) {
+		this.cardId = cardId;
+	}
+	
+	public int getLoyaltyPoint() {
+		return loyaltyPoint;
+	}
+	
+	public void setLoyaltyPoint(int loyaltyPoint) {
+		this.loyaltyPoint = loyaltyPoint;
+	}
+	
+	public String getMerchantAccNo() {
+		return merchantAccNo;
+	}
+	
+	public void setMerchantAccNo(String merchantAccNo) {
+		this.merchantAccNo = merchantAccNo;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
