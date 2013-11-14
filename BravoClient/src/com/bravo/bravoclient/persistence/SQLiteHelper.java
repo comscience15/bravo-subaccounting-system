ce7bb4d6e8be4e03c523e004479dd26c4a834baa
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2013-11-14 09:53:20 +0000

    initiate db

--- BravoClient/src/com/bravo/bravoclient/persistence/SQLiteHelper.java
+++ BravoClient/src/com/bravo/bravoclient/persistence/SQLiteHelper.java
@@ -1,5 +1,7 @@
 package com.bravo.bravoclient.persistence;
 
+import java.text.MessageFormat;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -7,15 +9,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SQLiteHelper extends SQLiteOpenHelper{
 	private static final String DATABASE_NAME = "BRAVO_DB";
 	private static final int DATABASE_VERSION = 1;
+	private static final String TABLE_NAME = "CARD";
+	private static final String COLUMN_CARD_ID = "CARD_ID";
+	private static final String COLUMN_LOYALTY_POINT = "LOYALTY_POINT";
+	private static final String COLUMN_MERCHANT_ACCOUNT_NUMBER = "MERCHANT_ACC_NO";
+	private static final String COLUMN_BALANCE = "BALANCE";
+	
+	private static final String CREATE_DB = "CREATE TABLE {1} ({2} TEXT PRIMARY KEY, {3} REAL, {4} TEXT, {5} REAL NOT NULL);";
+	
 	public SQLiteHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-		
+		db.execSQL(MessageFormat.format(CREATE_DB, 
+				new Object[] {TABLE_NAME, COLUMN_CARD_ID, COLUMN_LOYALTY_POINT, COLUMN_MERCHANT_ACCOUNT_NUMBER, COLUMN_BALANCE}));
 	}
 	
 	@Override
