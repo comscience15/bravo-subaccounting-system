6b1cd47169f573330917ab8c39a3624e90d0d1c1
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2013-11-28 09:39:58 +0000

    ClientAPICalls: add get card list api call

--- APICallsLibrary/src/com/bravo/https/apicalls/ClientAPICalls.java
+++ APICallsLibrary/src/com/bravo/https/apicalls/ClientAPICalls.java
@@ -1,5 +1,72 @@
 package com.bravo.https.apicalls;
 
-public class ClientAPICalls {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.apache.http.HttpResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.bravo.https.util.BravoHttpsClient;
+import com.bravo.https.util.HttpResponseHandler;
+
+import android.content.Context;
+
+public class ClientAPICalls {
+	private static Logger logger;
+	static {
+		logger = Logger.getLogger(ClientAPICalls.class.getName());
+	}
+	/**
+	 * 
+	 * @param IP
+	 * @param androidContext
+	 * @return
+	 * @throws KeyManagementException
+	 * @throws UnrecoverableKeyException
+	 * @throws CertificateException
+	 * @throws KeyStoreException
+	 * @throws NoSuchAlgorithmException
+	 * @throws IOException
+	 * @throws JSONException 
+	 */
+	public static ArrayList<JSONObject> getCardListByCustID(String IP, Context androidContext) 
+			throws KeyManagementException, UnrecoverableKeyException, CertificateException, KeyStoreException, NoSuchAlgorithmException, IOException, JSONException {
+		final String ip = IP;
+		final String path = "service/customer/account/getCardListByCustID";
+		final String URL = ip + path;
+		
+		FileInputStream getStoredCookie = null;
+		byte[] cookieByte = null;
+		int cookieSize = 0;
+		
+		try {
+			getStoredCookie = androidContext.openFileInput("Cookie");
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			cookieSize = getStoredCookie.available();
+			cookieByte = new byte[cookieSize];
+			getStoredCookie.read(cookieByte);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		String cookie = new String(cookieByte);
+		
+		HttpResponse response = BravoHttpsClient.doHttpsPost(URL, null, cookie, "getCardList", androidContext);
+		
+		return HttpResponseHandler.toArrayList(response);	
+	}
 }
