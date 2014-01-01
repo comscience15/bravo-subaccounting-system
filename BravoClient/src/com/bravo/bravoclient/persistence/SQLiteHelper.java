1f42c557b78596b14df6c6794494fe37d0694b28
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2014-01-01 11:44:10 +0000

    SQLiteHelper: Implement inserting card to database, and added column id for indexing

--- BravoClient/src/com/bravo/bravoclient/persistence/SQLiteHelper.java
+++ BravoClient/src/com/bravo/bravoclient/persistence/SQLiteHelper.java
@@ -14,12 +14,14 @@ public class SQLiteHelper extends SQLiteOpenHelper{
 	private static final String DATABASE_NAME = "BRAVO_DB";
 	private static final int DATABASE_VERSION = 1;
 	public static final String TABLE_NAME = "CARD";
+	public static final String COLUMN_ID = "COLUMN_ID";
 	public static final String COLUMN_CARD_ID = "CARD_ID";
 	public static final String COLUMN_LOYALTY_POINT = "LOYALTY_POINT";
 	public static final String COLUMN_MERCHANT_ACCOUNT_NUMBER = "MERCHANT_ACC_NO";
 	public static final String COLUMN_BALANCE = "BALANCE";
 	
-	private static final String CREATE_DB = "CREATE TABLE {0} ({1} TEXT PRIMARY KEY, {2} REAL, {3} TEXT, {4} REAL NOT NULL);";
+	private static final String CREATE_DB = "CREATE TABLE {0} ({1} INTEGER PRIMARY KEY 	AUTOINCREMENT, {2} TEXT NOT NULL, " +
+			"{3} REAL NOT NULL, {4} TEXT NOT NULL, {5} REAL NOT NULL, UNIQUE ({1}, {2}));";
 	
 	public SQLiteHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -28,7 +30,7 @@ public class SQLiteHelper extends SQLiteOpenHelper{
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(MessageFormat.format(CREATE_DB, 
-				new Object[] {TABLE_NAME, COLUMN_CARD_ID, COLUMN_LOYALTY_POINT, COLUMN_MERCHANT_ACCOUNT_NUMBER, COLUMN_BALANCE}));
+				new Object[] {TABLE_NAME, COLUMN_ID, COLUMN_CARD_ID, COLUMN_LOYALTY_POINT, COLUMN_MERCHANT_ACCOUNT_NUMBER, COLUMN_BALANCE}));
 		logger.log(Level.INFO, "DB is created");
 	}
 	
