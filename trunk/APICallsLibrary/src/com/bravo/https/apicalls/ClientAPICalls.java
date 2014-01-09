14b7ec1f4a614350b7d6071738bfa92e09863e6b
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2014-01-09 09:21:04 +0000

    Move getKey API from commonAPI to clientAPI

--- trunk/APICallsLibrary/src/com/bravo/https/apicalls/ClientAPICalls.java
+++ trunk/APICallsLibrary/src/com/bravo/https/apicalls/ClientAPICalls.java
@@ -3,12 +3,14 @@ package com.bravo.https.apicalls;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,6 +28,77 @@ public class ClientAPICalls {
 	static {
 		logger = Logger.getLogger(ClientAPICalls.class.getName());
 	}
+	
+	/**
+	 * Get public key API
+	 * @param IP
+	 * @return
+	 * @throws IOException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws KeyStoreException 
+	 * @throws CertificateException 
+	 * @throws UnrecoverableKeyException 
+	 * @throws KeyManagementException 
+	 */
+	public static Hashtable<String, BigInteger> getPublicKey(String IP, Context androidContext) 
+			throws KeyManagementException, UnrecoverableKeyException, CertificateException, KeyStoreException, NoSuchAlgorithmException, IOException {
+		final String ip = IP;
+		final String path = "service/customer/transaction/getKey";
+		final String URL = ip + path;
+		
+		FileInputStream getStoredCookie = null;
+		byte[] cookieByte = null;
+		int cookieSize = 0;
+		
+		try {
+			getStoredCookie = androidContext.openFileInput("Cookie");
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			cookieSize = getStoredCookie.available();
+			cookieByte = new byte[cookieSize];
+			getStoredCookie.read(cookieByte);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		String cookie = new String(cookieByte);
+		
+		System.out.println("getKey: Cookie is :" + cookie); // Debug
+		
+		HttpResponse response = BravoHttpsClient.doHttpsPost(URL, null, cookie, "getKey", androidContext);
+		
+		String responseString = HttpResponseHandler.toString(response);
+		String getKeyStatus = HttpResponseHandler.parseJson(responseString, "status");
+		
+		Hashtable<String, BigInteger> publickey = new Hashtable<String, BigInteger>();
+		
+		// Check if authentication is needed for getting keys
+		if (getKeyStatus != null && getKeyStatus.equals("404")) {
+			// Should go to login first
+			System.err.println("Should go to login first");
+			publickey.put("status", BigInteger.valueOf(404));
+			return null;
+		} else {
+			String e = HttpResponseHandler.parseJson(responseString, "e");
+			String n = HttpResponseHandler.parseJson(responseString, "n");
+			String maxdigits = HttpResponseHandler.parseJson(responseString, "maxdigits");
+		
+			System.out.println("e: " + e + " n: " + n + " max:" + maxdigits);
+		
+			BigInteger eInt = new BigInteger(e, 16);
+			BigInteger nInt = new BigInteger(n, 16);
+			BigInteger maxInt = new BigInteger(maxdigits, 16);
+			
+			publickey.put("e", eInt);
+			publickey.put("n", nInt);
+			publickey.put("maxidigits", maxInt);
+			return publickey;
+		}
+	}
+	
 	/**
 	 * This method is to get the cardList from server
 	 * The request should bundle with latest got cookie in order to pass the authentication
