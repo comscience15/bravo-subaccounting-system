d0bfd9fc5b60853d2803e4609ce59bd3e664fc7b
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2014-02-22 10:50:46 +0000

    Back end: Fixed Bug for 0 balance purchasing

--- trunk/subAccountAPI/src/main/java/com/bravo/webapp/transaction/CheckoutService.java
+++ trunk/subAccountAPI/src/main/java/com/bravo/webapp/transaction/CheckoutService.java
@@ -2,6 +2,8 @@ package com.bravo.webapp.transaction;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.text.MessageFormat;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Isolation;
@@ -22,23 +24,27 @@ public class CheckoutService {
 	@Autowired
 	private TransactionDAO transactionDAO;
 
+    private static final Logger logger = Logger.getLogger(CheckoutService.class.getName());
+
 	@Transactional(rollbackFor = Exception.class, isolation = Isolation.READ_UNCOMMITTED)
 	public Transaction checkoutItems(String cardID, Transaction transaction) {
 
 		String merchantAccNo = LoginInformation.getMerchantAccNo();
 		transaction.setMerchantAccNo(merchantAccNo);
 		BigDecimal totalAmount = transaction.getTotalAmount();
-		transaction.setTotalAmount(totalAmount.negate());
+		transaction.setTotalAmount(totalAmount.negate());  // User purchase, the record in DB is negative
 		Timestamp merchantTimestamp = transaction.getTransactionDate();
 
 		// Get the card information
 		Card card = cardDAO.getCard(merchantAccNo, cardID);
 		if (card == null) {
+            logger.warning("Failed to fetch the card");
 			throw new UnknownResourceException("Failed to fetch the card.");
 		}
 
 		// Check if card is enabled
 		if (!card.isEnabled()) {
+            logger.warning("Card is disabled");
 			throw new UnknownResourceException("Card is disabled");
 		}
 
@@ -46,19 +52,23 @@ public class CheckoutService {
 		// Check if customer is the card owner.
 		if (customerID != null) {
 			if (!card.getCustomerID().equals(customerID)) {
+                logger.warning("Wrong card owner");
 				throw new UnknownResourceException("Wrong card owner.");
 			}
 		} else {
 			Timestamp customerTimestamp = transaction.getCustomerTimestamp();
 			// Check if customer timestamp was used before
 			if (!transactionDAO.isTimestampUnique(cardID, customerTimestamp)) {
+                logger.warning("Customer timestamp is incorrect");
 				throw new UnknownResourceException("Timestamp is not unique");
 			}
-
 		}
 
 		// Check if balance is sufficient
-		if (card.getBalance().compareTo(transaction.getTotalAmount()) < 0) {
+		if (card.getBalance().compareTo(totalAmount) < 0) {
+            String msg =  MessageFormat.format("Insufficient balance, card balance is {0}, transaction amount is {1}",
+                    new Object[] {card.getBalance(), totalAmount});
+            logger.warning(msg);
 			throw new UnknownResourceException("Insufficient balance");
 		}
 
@@ -72,10 +82,10 @@ public class CheckoutService {
 
 		// Add transaction record
 		long lastTransactionID = transactionDAO.getLastTransactionID();
-		System.out.println("transactionID: " + lastTransactionID);
+        logger.info("transactionID: " + lastTransactionID);
 		Integer receiptNo = (int) lastTransactionID;
 		transaction.setReceiptNo(receiptNo);
-		System.out.println("receiptNo: " + receiptNo);
+        logger.info("receiptNo: " + receiptNo);
 		if (!transactionDAO.addTransaction(transaction)) {
 			throw new UnknownResourceException("Add transaction failed");
 		}
