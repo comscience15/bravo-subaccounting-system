e18d056b15e9f0d083660d2e3c2ab429db830453
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2014-02-23 09:12:07 +0000

    Added client reload money feature

---
+++ BravoClient/src/com/bravo/bravoclient/async/AsyncReloadMoney.java
@@ -0,0 +1,101 @@
+package com.bravo.bravoclient.async;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.logging.Logger;
+
+import org.apache.http.NameValuePair;
+
+import com.bravo.bravoclient.R;
+import com.bravo.bravoclient.activities.MainActivity;
+import com.bravo.bravoclient.dialogs.BravoAlertDialog;
+import com.bravo.https.apicalls.ClientAPICalls;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+/**
+ * Async task for reloading money
+ * @author daniel
+ *
+ */
+public class AsyncReloadMoney extends AsyncTask<Object, Void, Hashtable<String, String>>{
+	private Context context;
+	private static final Logger logger = Logger.getLogger(AsyncReloadMoney.class.getName());
+	
+	public AsyncReloadMoney(Context context) {
+		this.context = context;
+	}
+	
+	/**
+	 * params[0] is IP address
+	 * params[1] is parameters for api call
+	 */
+	@Override
+	protected Hashtable<String, String> doInBackground(Object... params) {
+		String ip;
+		ArrayList<NameValuePair> apiParaList;
+		Hashtable<String, String> APIResponse = new Hashtable<String, String>();
+		if (params[1] instanceof ArrayList && params[0] instanceof String) {
+			apiParaList = (ArrayList<NameValuePair>) params[1];
+			ip = (String) params[0];
+		} else {
+			logger.warning("Parameters for api call are invalid");
+			return null;
+		}
+		
+		try {
+			APIResponse = ClientAPICalls.reloadMoneyByCreditCard(ip, context, apiParaList);
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnrecoverableKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return APIResponse;
+	}
+	
+	/**
+	 * @param The parameter is from doInBackground()
+	 */
+	@Override
+	protected void onPostExecute(Hashtable<String, String> result) {
+		if (result != null && (result.get("status").equals("404")) == false) {
+			String msg = MessageFormat.format("Reloading card successful. New balance is $ {0}", result.get("message"));
+			Toast.makeText(context,msg, Toast.LENGTH_SHORT).show();
+			
+			Intent toCardsFragment = new Intent(context, MainActivity.class);
+			toCardsFragment.putExtra("Activity", "ReloadMoney");
+			toCardsFragment.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	    	context.startActivity(toCardsFragment);
+	    	((Activity) context).overridePendingTransition(R.anim.go_back_enter, R.anim.go_back_out);
+		} else {
+			new BravoAlertDialog(context).showDialog("Reload Money Failed", result.get("message"), "OK");
+		}
+	}
+
+}
