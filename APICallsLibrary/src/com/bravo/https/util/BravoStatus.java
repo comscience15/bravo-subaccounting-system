a2a85a6668d2777330b8fdf028ea667e3c38930b
danniel1205@gmail.com <danniel1205@gmail.com@c22c7f27-d223-dc22-4c5b-4bc0d4420971>
2014-02-23 09:15:32 +0000

    update BravoStatus static values

--- APICallsLibrary/src/com/bravo/https/util/BravoStatus.java
+++ APICallsLibrary/src/com/bravo/https/util/BravoStatus.java
@@ -1,6 +1,6 @@
 package com.bravo.https.util;
 
 public class BravoStatus {
-	public static final String AUTHENTICATION_FAILED = "404";
+	public static final String OPERATION_FAILED = "404";
 	public static final String OPERATION_SUCCESS = "200";
 }
